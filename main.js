49d7081a78cb599d6e0b14fa36e43560dc66b24b
stevenbanilbo <89102842+stevenbanilbo@users.noreply.github.com>
2021-08-18 12:28:02 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function toggleDropdown(){
+
+    let navbarToggle = document.getElementById("navbar-toggle");
+    if(navbarToggle.className ==='topnav'){
+
+        navbarToggle.className += " responsive";
+
+    }
+    else
+    {
+    navbarToggle.className = 'topnav';
+
+    }
+}
